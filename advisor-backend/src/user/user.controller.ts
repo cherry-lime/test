ba96c486e04abcadc10e1740642747e0f5cc891f
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-05-18 10:57:19 +0200

    Add specification for [GET] /user/:id

--- advisor-backend/src/user/user.controller.ts
+++ advisor-backend/src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  /**
+   * [GET] /user/:id
+   * @param id user_id
+   * @returns user object
+   */
+  @Get(':id')
+  getUser(@Param('id', ParseUUIDPipe) id: string) {
+    return this.userService.getUser(id);
+  }
+}
--- advisor-backend/src/user/user.service.ts
+++ advisor-backend/src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * Get user object by uuid
+   * @param uuid uuid of user
+   * @returns user object corresponding to userid, null if not found
+   */
+  async getUser(uuid: string) {
+    return;
+  }
+}
