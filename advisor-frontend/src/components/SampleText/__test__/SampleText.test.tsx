6fa9d47da63d8ce564073ea47083bf50e4781020
lukasagurkas <57620511+lukasagurkas@users.noreply.github.com>
2022-05-19 10:19:12 +0200

    Fixed SampleText test case (frontend)

--- advisor-frontend/src/components/SampleText/__test__/SampleText.test.tsx
+++ advisor-frontend/src/components/SampleText/__test__/SampleText.test.tsx
@@ -5,7 +5,8 @@ describe('when rendered with a message as a prop', () => {
   it('should paste into the message text', () => {
     render(<Greetings name="Test Name" />);
     expect(
-      screen.getByText(/Hello Test Name!/),
+      screen.getByText('Hello', {exact: false}),
+      //screen.getByText(/Hello Test Name!/),
     ).toBeInTheDocument();
   });
 });
