e3c86dc303bdb8ad8b75617662a558b8600d9793
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-05-20 13:53:07 +0200

    Add automatic OpenAPI documentation for backend

--- advisor-backend/src/main.ts
+++ advisor-backend/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TestING Advisor API')
+    .setDescription('The TestING Advisor API description')
+    .setVersion('1.0')
+    .addTag('testadvisor')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
