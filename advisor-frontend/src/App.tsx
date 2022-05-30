fd1c133b1998880b04eacde7c9b1466af2a513da
Dragonsky8 <Williamiphone3@gmail.com>
2022-05-30 11:58:14 +0200

    Added the sidebar component

--- advisor-frontend/src/App.tsx
+++ advisor-frontend/src/App.tsx
@@ -3,27 +3,12 @@ import logo from '/src/logo.svg';
 import './App.css';
 import {Greetings} from './components/SampleText/SampleText';
 import {Button} from './components/Button/Button';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-        <Greetings name = "This is a test to test a component"></Greetings>
-        <Button name = "Buttontext"></Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
     </div>
   );
 }
