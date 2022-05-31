55d568ec2f7bbdbd4c42a5a462337c3cfedfef0c
cherry-lime <hatorisugawara@gmail.com>
2022-05-31 11:20:50 +0200

    Add role validator during registration

--- advisor-backend/src/auth/dto/register-user.dto.ts
+++ advisor-backend/src/auth/dto/register-user.dto.ts
@@ -1,12 +1,13 @@
 import { Role } from '@prisma/client';
 import { IsIn } from 'class-validator';
 
+
 export class CreateUserDto {
   //@IsString()
   //@Length(1, 10)
   //username: string;
 
   //@Length(1, 10)
-  //@IsIn([Role.ASSESSOR, Role.USER])
-  role: Role;
+  @IsIn([Role.ASSESSOR, Role.USER])
+  roles: Role;
 }
--- advisor-backend/src/main.ts
+++ advisor-backend/src/main.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(PORT);
 }
 bootstrap();
