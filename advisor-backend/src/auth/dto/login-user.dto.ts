5a2a940d893eac06a5d1d40bc6e0f3488077aa43
cherry-lime <hatorisugawara@gmail.com>
2022-05-31 11:24:15 +0200

    Resolving conflicts

--- advisor-backend/src/auth/dto/login-user.dto.ts
+++ advisor-backend/src/auth/dto/login-user.dto.ts
@@ -1,9 +1,10 @@
 import { IsString } from 'class-validator';
+// import { IsUUID } from 'class-validator';
 
 export class LoginDto {
   @IsString()
   username: string;
 
-  @IsString()
+  // @IsUUID()
   password_hash: string;
 }
--- advisor-backend/src/auth/dto/register-user.dto.ts
+++ advisor-backend/src/auth/dto/register-user.dto.ts
@@ -1,13 +1,8 @@
 import { Role } from '@prisma/client';
 import { IsIn } from 'class-validator';
 
-
 export class CreateUserDto {
-  //@IsString()
-  //@Length(1, 10)
-  //username: string;
 
-  //@Length(1, 10)
   @IsIn([Role.ASSESSOR, Role.USER])
   roles: Role;
 }
