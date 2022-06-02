77eb99a4d794b245ed25296f2e81366f39e9ea16
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-02 09:45:26 +0200

    Restructure prisma mock

---
+++ advisor-backend/src/prisma/mock/mockTemplate.ts
@@ -0,0 +1,14 @@
+export const aTemplate = {
+  template_id: 1,
+  template_name: 'test',
+  template_type: 'INDIVIDUAL',
+  disabled: false,
+  weight_range_min: 1,
+  weight_range_max: 3,
+  score_formula: 'sum(x)',
+  include_no_answer: false,
+};
+
+export const mockTemplate = {
+  create: jest.fn().mockResolvedValue(aTemplate),
+};
--- advisor-backend/src/user/user.service.spec.ts
+++ advisor-backend/src/user/user.service.spec.ts
@@ -2,36 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../prisma/prisma.service';
 import { UserService } from './user.service';
 import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { mockPrisma } from '../prisma/mock/mockPrisma';
+import { aUser } from '../prisma/mock/mockUser';
 
 const moduleMocker = new ModuleMocker(global);
-
-// Mock users array
-const userArray = [
-  {
-    user_id: 1,
-    password_hash: 'fdsfdsfds',
-  },
-  {
-    user_id: 2,
-    password_hash: 'fdsfdsdfdfdsdsfss',
-  },
-];
-// Mock single user
-const aUser = userArray[0];
-
-// Mock prisma service
-const mockPrisma = {
-  user: {
-    findMany: jest.fn().mockResolvedValue(userArray),
-    findUnique: jest.fn().mockResolvedValue(aUser),
-    findFirst: jest.fn().mockResolvedValue(aUser),
-    findOne: jest.fn().mockResolvedValue(aUser),
-    create: jest.fn().mockResolvedValue(aUser),
-    save: jest.fn(),
-    update: jest.fn().mockResolvedValue(aUser),
-    delete: jest.fn().mockResolvedValue(aUser),
-  },
-};
 
 describe('UserService', () => {
   let userService: UserService;
