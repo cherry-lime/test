efe1c174c968bf0ee6a05b2bec111e6bdcdf1c3b
Adrian <a.a.t.beers@student.tue.nl>
2022-06-03 13:38:19 +0200

    Fix indentation
    Fixes indentation in ExampleGrid.tsx, GenericGrid.tsx, GenericGrid.css.

--- advisor-frontend/src/components/Grids/ExampleGrid.tsx
+++ advisor-frontend/src/components/Grids/ExampleGrid.tsx
@@ -1,193 +1,189 @@
 import * as React from "react";
 import { createTheme } from "@mui/material/styles";
-import { 
-    GridActionsCellItem,
-    GridColumns,
-    GridRenderCellParams, 
-    GridRowId} 
-from "@mui/x-data-grid";
+import {
+  GridActionsCellItem,
+  GridColumns,
+  GridRenderCellParams,
+  GridRowId
+} from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
 
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import DeleteIcon from '@mui/icons-material/Delete';
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import GenericGrid from "./GenericGrid";
 
 const theme = createTheme({
-    // Style color palette
-    palette: {
-        primary: {
-            light: "#FFD6B1", // Light Orange
-            main: "#FF6200", // Orange
-            dark: "#AA3909" // Dark Orange
-        },
-        secondary: {
-            // Lightest Grey: #FAF6F3
-            light: "#EDE6E2", // Light Grey
-            main: "#8B817C", // Grey
-            dark: "#5A534F" // Dark Grey
-        },
-        text: {
-            primary: "#5A534F" // Dark Grey
-        }
+  // Style color palette
+  palette: {
+    primary: {
+      light: "#FFD6B1", // Light Orange
+      main: "#FF6200", // Orange
+      dark: "#AA3909" // Dark Orange
+    },
+    secondary: {
+      // Lightest Grey: #FAF6F3
+      light: "#EDE6E2", // Light Grey
+      main: "#8B817C", // Grey
+      dark: "#5A534F" // Dark Grey
+    },
+    text: {
+      primary: "#5A534F" // Dark Grey
     }
+  }
 });
 
 type Row = {
-    id: number,
-    name: string,
-    date: Date
-}
-  
+  id: number;
+  name: string;
+  date: Date;
+};
+
 // Initial rows that are rendered in the grid
 const initialRows: Row[] = [
-    {
-        id: 1,
-        name: "Alice",
-        date: new Date(1979, 0, 1)
-    },
-    {
-        id: 2,
-        name: "Bob",
-        date: new Date(1984, 1, 1)
-    },
-    {
-        id: 3,
-        name: "Charlie",
-        date: new Date(1992, 2, 1)
-    },
-    {
-        id: 4,
-        name: "Denise",
-        date: new Date(1992, 2, 1)
-    }
+  {
+    id: 1,
+    name: "Alice",
+    date: new Date(1979, 0, 1)
+  },
+  {
+    id: 2,
+    name: "Bob",
+    date: new Date(1984, 1, 1)
+  },
+  {
+    id: 3,
+    name: "Charlie",
+    date: new Date(1992, 2, 1)
+  },
+  {
+    id: 4,
+    name: "Denise",
+    date: new Date(1992, 2, 1)
+  }
 ];
-  
+
 export default function ExampleGrid() {
-    const [rows, setRows] = React.useState<Row[]>(initialRows);
+  const [rows, setRows] = React.useState<Row[]>(initialRows);
 
-    // Generate new id based on time
-    const generateId = () => Date.now();
+  // Generate new id based on time
+  const generateId = () => Date.now();
 
-    // Called when "Add" button is pressed below the grid
-    const handleAdd = () => {
-        setRows((prevRows) => {
-            // Create new id
-            const newId = generateId();
-            console.log(`Adding row with id ${newId} to database`);
+  // Called when "Add" button is pressed below the grid
+  const handleAdd = () => {
+    setRows((prevRows) => {
+      // Create new id
+      const newId = generateId();
+      console.log(`Adding row with id ${newId} to database`);
 
-            // Create new row with default content
-            const newRow = {id: newId, name: "name", date: new Date(0, 0, 0)};
-            return [...prevRows, newRow];
-        });
-    };
+      // Create new row with default content
+      const newRow = { id: newId, name: "name", date: new Date(0, 0, 0) };
+      return [...prevRows, newRow];
+    });
+  };
 
-    // Called when the "Delete" action is pressed in the action menu
-    const handleDelete = React.useCallback(
-        (rowId: GridRowId) => () => {
-            // Use setTimeout to deal with delay
-            setTimeout(() => {
-                // Filter row with rowId from state
-                setRows((prevRows) => prevRows.filter((row) => row.id !== rowId));
+  // Called when the "Delete" action is pressed in the action menu
+  const handleDelete = React.useCallback(
+    (rowId: GridRowId) => () => {
+      // Use setTimeout to deal with delay
+      setTimeout(() => {
+        // Filter row with rowId from state
+        setRows((prevRows) => prevRows.filter((row) => row.id !== rowId));
 
-                // Update database to remove row with rowId
-                console.log(`Removing row with id ${rowId} from database`);
-            });
-        },
-        []
-    );
+        // Update database to remove row with rowId
+        console.log(`Removing row with id ${rowId} from database`);
+      });
+    },
+    []
+  );
 
-    // Called when the "Duplicate" action is pressed in the action menu
-    const handleDuplicate = React.useCallback(
-        (rowId: GridRowId) => () => {
-            setRows((prevRows) => {
-                // Get the row with rowId
-                const rowToDuplicate = prevRows.find((row) => row.id === rowId)!;
+  // Called when the "Duplicate" action is pressed in the action menu
+  const handleDuplicate = React.useCallback(
+    (rowId: GridRowId) => () => {
+      setRows((prevRows) => {
+        // Get the row with rowId
+        const rowToDuplicate = prevRows.find((row) => row.id === rowId)!;
 
-                // Create new id
-                const newId = generateId();
-                console.log(`Adding row with id ${newId} to database`);
+        // Create new id
+        const newId = generateId();
+        console.log(`Adding row with id ${newId} to database`);
 
-                // Create new row with duplicated content
-                const newRow = { ...rowToDuplicate, id: newId }
-                return [...prevRows, newRow];
-            });
-        },
-        []
-    );
+        // Create new row with duplicated content
+        const newRow = { ...rowToDuplicate, id: newId };
+        return [...prevRows, newRow];
+      });
+    },
+    []
+  );
 
-    const columns = React.useMemo<GridColumns<Row>>(
-        () => [
-            {
-                field: "name",
-                headerName: "Name",
-                flex: 1,
-                editable: true
-            },
-            {
-                field: "date",
-                headerName: "Year",
-                flex: 1,
-                renderCell: (params: GridRenderCellParams<Date>) => (
-                    <strong>
-                        {params.value?.getFullYear() ?? ""}
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            size="small"
-                            style={{ marginLeft: 16 }}
-                        >
-                            Open
-                        </Button>
-                    </strong>
-                )
-            },
-            {
-                field: 'actions',
-                type: 'actions',
-                width: 350,
-                getActions: (params) => [
-                    <Button
-                        variant="contained"
-                    >
-                        <strong>Contained</strong>
-                    </Button>,
-                    <Button
-                        variant="outlined"
-                    >
-                        <strong>Outlined</strong>
-                    </Button>,
-                    <GridActionsCellItem
-                        icon={<ArrowForwardIcon />}
-                        label="Visit"
-                    />,
-                    <GridActionsCellItem
-                        icon={<FileCopyIcon />}
-                        label="Duplicate"
-                        onClick={handleDuplicate(params.id)}
-                        showInMenu
-                    />,
-                    <GridActionsCellItem
-                        icon={<DeleteIcon />}
-                        label="Delete"
-                        onClick={handleDelete(params.id)}
-                        showInMenu
-                    />
-                ]
-            }
-        ],
-        [handleDuplicate, handleDelete]
-    );
+  const columns = React.useMemo<GridColumns<Row>>(
+    () => [
+      {
+        field: "name",
+        headerName: "Name",
+        flex: 1,
+        editable: true
+      },
+      {
+        field: "date",
+        headerName: "Year",
+        flex: 1,
+        renderCell: (params: GridRenderCellParams<Date>) => (
+          <strong>
+            {params.value?.getFullYear() ?? ""}
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              style={{ marginLeft: 16 }}
+            >
+              Open
+            </Button>
+          </strong>
+        )
+      },
+      {
+        field: "actions",
+        type: "actions",
+        width: 350,
+        getActions: (params) => [
+          <Button variant="contained">
+            <strong>Contained</strong>
+          </Button>,
+          <Button variant="outlined">
+            <strong>Outlined</strong>
+          </Button>,
+          <GridActionsCellItem 
+            icon={<ArrowForwardIcon />} 
+            label="Visit" 
+          />,
+          <GridActionsCellItem
+            icon={<FileCopyIcon />}
+            label="Duplicate"
+            onClick={handleDuplicate(params.id)}
+            showInMenu
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDelete(params.id)}
+            showInMenu
+          />
+        ]
+      }
+    ],
+    [handleDuplicate, handleDelete]
+  );
 
-    return (
-        <GenericGrid
-            theme={theme}
-            rows={rows}
-            columns={columns}
-            hasToolbar
-            textAdd="add user"
-            handleAdd={handleAdd}
-        />
-    );
+  return (
+    <GenericGrid
+      theme={theme}
+      rows={rows}
+      columns={columns}
+      hasToolbar
+      textAdd="add user"
+      handleAdd={handleAdd}
+    />
+  );
 }
--- advisor-frontend/src/components/Grids/GenericGrid.tsx
+++ advisor-frontend/src/components/Grids/GenericGrid.tsx
@@ -1,83 +1,93 @@
 import * as React from "react";
-import { DataGrid, GridToolbar, gridClasses, GridRowClassNameParams } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridToolbar,
+  gridClasses,
+  GridRowClassNameParams
+} from "@mui/x-data-grid";
 import { styled, ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import "./GenericGrid.css";
 
 const StyledGrid = styled(DataGrid)(({ theme }) => ({
-    // Style even rows
-    [`& .${gridClasses.row}.even`]: {
-        backgroundColor: "white", // White
-        "&:hover, &.Mui-hovered": {
-            backgroundColor: theme.palette.primary.light, // Light Orane
-            "@media (hover: none)": {
-                backgroundColor: "transparent"
-            }
-        }
-    },
-    // Style odd rows
-    [`& .${gridClasses.row}.odd`]: {
-        backgroundColor: theme.palette.secondary.light, // Light Grey
-        "&:hover, &.Mui-hovered": {
-            backgroundColor: theme.palette.primary.light, // Light Orange
-            "@media (hover: none)": {
-                backgroundColor: "transparent"
-            }
-        }
-    },
-    // Style toolbar 
-    "& .MuiDataGrid-virtualScrollerContent": {
-        backgroundColor: "white" // White
-    },
-    // Style header
-    "& .MuiDataGrid-columnHeaders": {
-        color: theme.palette.secondary.light, // Light Grey
-        backgroundColor: theme.palette.secondary.dark // Dark Grey
-    },
-    // Style footer
-    "& .MuiDataGrid-footerContainer": {
-        backgroundColor: "white" // Dark Grey
-    },
-    // Style sort icon (arrow in header)
-    "& .MuiDataGrid-sortIcon": {
-        color: theme.palette.secondary.light // Light Grey
-    },
-    // Style menu icon button (triple dots in header)
-    "& .MuiDataGrid-menuIconButton": {
-        color: theme.palette.secondary.light // Light Grey
+  // Style even rows
+  [`& .${gridClasses.row}.even`]: {
+    backgroundColor: "white", // White
+    "&:hover, &.Mui-hovered": {
+      backgroundColor: theme.palette.primary.light, // Light Orane
+      "@media (hover: none)": {
+        backgroundColor: "transparent"
+      }
     }
+  },
+  // Style odd rows
+  [`& .${gridClasses.row}.odd`]: {
+    backgroundColor: theme.palette.secondary.light, // Light Grey
+    "&:hover, &.Mui-hovered": {
+      backgroundColor: theme.palette.primary.light, // Light Orange
+      "@media (hover: none)": {
+        backgroundColor: "transparent"
+      }
+    }
+  },
+  // Style toolbar
+  "& .MuiDataGrid-virtualScrollerContent": {
+    backgroundColor: "white" // White
+  },
+  // Style header
+  "& .MuiDataGrid-columnHeaders": {
+    color: theme.palette.secondary.light, // Light Grey
+    backgroundColor: theme.palette.secondary.dark // Dark Grey
+  },
+  // Style footer
+  "& .MuiDataGrid-footerContainer": {
+    backgroundColor: "white" // Dark Grey
+  },
+  // Style sort icon (arrow in header)
+  "& .MuiDataGrid-sortIcon": {
+    color: theme.palette.secondary.light // Light Grey
+  },
+  // Style menu icon button (triple dots in header)
+  "& .MuiDataGrid-menuIconButton": {
+    color: theme.palette.secondary.light // Light Grey
+  }
 }));
 
 export default function GenericGrid({
-    theme,
-    rows,
-    columns,
-    hasToolbar,
-    textAdd,
-    handleAdd
+  theme,
+  rows,
+  columns,
+  hasToolbar,
+  textAdd,
+  handleAdd
 }: any) {
-    return (
-        <ThemeProvider theme={theme}>
-            <div className="GenericGrid" style={{ backgroundColor: theme.palette.secondary.dark }}>
-                <StyledGrid
-                    rows={rows}
-                    columns={columns}
-                    components={hasToolbar && { Toolbar: GridToolbar }}
-                    // getRowHeight={() => 'auto'}
-                    getRowClassName={(params: GridRowClassNameParams) =>
-                        params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
-                    }
-                />
-                {textAdd && handleAdd && ( // if addText and addFunc not null
-                    <Button
-                        onClick={handleAdd}
-                        variant="outlined"
-                        style={{ width: "100%", backgroundColor: "white" }}
-                    >
-                        <strong>+ {textAdd}</strong>
-                    </Button>
-                )}
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <div
+        className="GenericGrid"
+        style={{ backgroundColor: theme.palette.secondary.dark }}
+      >
+        <StyledGrid
+          rows={rows}
+          columns={columns}
+          components={hasToolbar && { Toolbar: GridToolbar }}
+          // onRowEditCommit={}
+          // getRowHeight={() => 'auto'}
+          getRowClassName={(params: GridRowClassNameParams) =>
+            params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
+          }
+        />
+        {textAdd &&
+        handleAdd && ( // if addText and addFunc not null
+            <Button
+              onClick={handleAdd}
+              variant="outlined"
+              style={{ width: "100%", backgroundColor: "white" }}
+            >
+              <strong>+ {textAdd}</strong>
+            </Button>
+          )}
+      </div>
+    </ThemeProvider>
+  );
 }
