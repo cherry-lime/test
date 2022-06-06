b4eb10abecf708611bce18636b6a98fc2487ec96
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-06 20:35:01 +0200

    Resolve merge conflict

--- advisor-backend/src/prisma/mock/mockPrisma.ts
+++ advisor-backend/src/prisma/mock/mockPrisma.ts
@@ -1,9 +1,6 @@
 import { mockAssessment } from './mockAssessment';
 import { mockCategory } from './mockCategory';
-<<<<<<< HEAD
 import { mockAssessmentParticipants } from './mockAssessmentParticipants';
-=======
->>>>>>> 9f25508a3db08e7641b00f5ccefa42a7bd72e255
 import { mockMaturity } from './mockMaturity';
 import { mockTeam } from './mockTeam';
 import { mockTemplate } from './mockTemplate';
@@ -15,9 +12,6 @@ export const mockPrisma = {
   assessment: mockAssessment,
   team: mockTeam,
   category: mockCategory,
-<<<<<<< HEAD
   assessmentParticipants: mockAssessmentParticipants,
-=======
->>>>>>> 9f25508a3db08e7641b00f5ccefa42a7bd72e255
   maturity: mockMaturity,
 };
