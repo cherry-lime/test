4344f6f3656557eeca11fb8db980a37083c6573e
Hichub <64192530+Hichub@users.noreply.github.com>
2022-06-07 18:41:50 +0200

    Removing some comments in user.service

--- advisor-backend/src/user/user.service.ts
+++ advisor-backend/src/user/user.service.ts
@@ -6,10 +6,8 @@ import {
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './../auth/dto/register-user.dto';
 import { Prisma, PrismaClient, User } from '../../node_modules/.prisma/client';
-// import { bcrypt } from '../../node_modules/bcrypt'
 import * as bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
-// import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class UserService {
@@ -33,10 +31,6 @@ export class UserService {
     const randomWords = require('random-words');
     const new_username = randomWords({ min: 2, max: 3, join: '_' });
 
-    // const prisma = new PrismaClient();
-    // let users: Prisma.UserCreateInput;
-
-    // TODO: make password hash work
     const existing = await this.prismaService.user.findUnique({
       where: {
         username: new_username,
@@ -62,17 +56,4 @@ export class UserService {
     delete user.password_hash;
     return user;
   }
-
-  /**
-   * Get user object by id
-   * @param id id of user
-   * @returns user object corresponding to user_id, null if not found
-   */
-  //   async getUser(id: number): Promise<any> {
-  //     return await this.prisma.user.findFirst({
-  //       where: {
-  //         user_id: id,
-  //       },
-  //     });
-  //   }
 }
