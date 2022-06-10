a6be8b6164138872fc3e7eae495935606c034222
cherry-lime <hatorisugawara@gmail.com>
2022-06-10 14:07:03 +0200

    Remove uuid check because passwords are hashed

--- advisor-backend/src/auth/dto/login-user.dto.ts
+++ advisor-backend/src/auth/dto/login-user.dto.ts
@@ -1,11 +1,10 @@
-import { IsString, IsUUID } from 'class-validator';
+import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 export class LoginDto {
   @IsString()
   @ApiProperty()
   username: string;
 
-  @IsUUID()
   @ApiProperty()
   password: string;
 }
