1b77f2baf73255b1fb10d6e6371db9ec7376af24
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-13 13:39:00 +0200

    Implement maturity order updating

--- advisor-backend/src/app.module.ts
+++ advisor-backend/src/app.module.ts
@@ -7,6 +7,7 @@ import { UserModule } from './user/user.module';
 import { AssessmentModule } from './assessment/assessment.module';
 import { TemplateModule } from './template/template.module';
 import { CategoryModule } from './category/category.module';
+import { MaturityModule } from './maturity/maturity.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { CategoryModule } from './category/category.module';
     TemplateModule,
     AssessmentModule,
     CategoryModule,
+    MaturityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
