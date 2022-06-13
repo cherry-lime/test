edd93122ffff1208c700a19293d5532a32e49cd0
ian100000 <59604216+ian100000@users.noreply.github.com>
2022-06-13 18:29:31 +0200

    Update Textfield.test.tsx

--- advisor-frontend/src/components/Textfield/__test__/Textfield.test.tsx
+++ advisor-frontend/src/components/Textfield/__test__/Textfield.test.tsx
@@ -6,6 +6,6 @@ afterEach(cleanup);
 
 //  test rendering of the noneditable textfield and check of bodytext
 it("Rendering without crash and check placeholdertext", () => {
-  const { getByText } = render(<Textfield bodytext2="lorem ipsum" />);
+  const { getByText } = render(<Textfield text="lorem ipsum" />);
   expect(getByText("lorem ipsum")).toBeInTheDocument();
 });
