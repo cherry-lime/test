e78d1a535bac45fd92709968ac9d6e50938111ec
ian100000 <59604216+ian100000@users.noreply.github.com>
2022-06-13 18:32:13 +0200

    Update TextfieldEdit.test.tsx

--- advisor-frontend/src/components/TextfieldEdit/__test__/TextfieldEdit.test.tsx
+++ advisor-frontend/src/components/TextfieldEdit/__test__/TextfieldEdit.test.tsx
@@ -6,13 +6,13 @@ afterEach(cleanup);
 
 //  test rendering of the editable textfield and check of bodytext
 it("Rendering without crash", () => {
-  const { getByText } = render(<TextfieldEdit bodytext="lorem ipsum" />);
+  const { getByText } = render(<TextfieldEdit text="lorem ipsum" />);
   expect(getByText("lorem ipsum")).toBeInTheDocument();
 });
 
 //  test the change of the bodytext after editing in the textfield
 it("Check input change", () => {
-  const { getByText } = render(<TextfieldEdit bodytext="lorem ipsum" />);
+  const { getByText } = render(<TextfieldEdit text="lorem ipsum" />);
   const inputElement = getByText("lorem ipsum") as HTMLInputElement;
   fireEvent.change(inputElement, { target: { value: "lorem ipsum changed" } });
   expect(getByText("lorem ipsum changed")).toBeInTheDocument();
