16ed5e48e65aed8da5c1e6a4c0df86083e94c4fd
ian100000 <59604216+ian100000@users.noreply.github.com>
2022-06-13 18:24:18 +0200

    Update PageCard.test.tsx

--- advisor-frontend/src/components/PageCard/__test__/PageCard.test.tsx
+++ advisor-frontend/src/components/PageCard/__test__/PageCard.test.tsx
@@ -6,7 +6,16 @@ afterEach(cleanup);
 
 //  test rendering of the home page card with bodytext and title
 it("Rendering without crash and check title and text on card", () => {
-  const { getByText } = render(<PageCard bodytext3="lorem ipsum" />);
-  expect(getByText("lorem ipsum")).toBeInTheDocument();
-  expect(getByText("Individual Evaluation")).toBeInTheDocument();
+  const { getByText } = render(
+    <PageCard
+      headerText="This is a description for the home page card"
+      bodyText="Title"
+      icon={undefined}
+      image="image"
+    />
+  );
+  expect(
+    getByText("This is a description for the home page card")
+  ).toBeInTheDocument();
+  expect(getByText("Title")).toBeInTheDocument();
 });
