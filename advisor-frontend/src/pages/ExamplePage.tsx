aa22d3433f8645e8ea68841f6fc23a528e848b66
Dragonsky8 <Williamiphone3@gmail.com>
2022-06-14 22:10:20 +0200

    Updated default header color.

--- advisor-frontend/src/pages/ExamplePage.tsx
+++ advisor-frontend/src/pages/ExamplePage.tsx
@@ -35,7 +35,6 @@ export default function Example() {
     <PageLayout
       title="Home"
       footer
-      headerColor="#9f9dc4"
       sidebarType={userType.USER}
     >
       <Stack spacing={3}>
