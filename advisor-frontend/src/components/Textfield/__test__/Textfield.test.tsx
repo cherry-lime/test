29117055a8b3b66c466e6cd19d21b00418ec1476
ian100000 <ianvandewetering@gmail.com>
2022-06-15 16:41:38 +0200

    fix issues in testing
    fix issues in testing

--- advisor-frontend/src/components/Textfield/__test__/Textfield.test.tsx
+++ advisor-frontend/src/components/Textfield/__test__/Textfield.test.tsx
@@ -1,11 +1,26 @@
+import { createTheme } from "@mui/material";
 import { render, cleanup } from "@testing-library/react";
 import Textfield from "../Textfield";
 
 //  cleanup after each test case
 afterEach(cleanup);
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff6200",
+    },
+    secondary: {
+      main: "#5a534f",
+    },
+    text: {
+      primary: "#5a534f",
+    },
+  },
+});
+
 //  test rendering of the noneditable textfield and check of bodytext
 it("Rendering without crash and check placeholdertext", () => {
-  const { getByText } = render(<Textfield text="lorem ipsum" />);
+  const { getByText } = render(<Textfield text="lorem ipsum" theme={theme} />);
   expect(getByText("lorem ipsum")).toBeInTheDocument();
 });
--- advisor-frontend/src/components/TextfieldEdit/__test__/TextfieldEdit.test.tsx
+++ advisor-frontend/src/components/TextfieldEdit/__test__/TextfieldEdit.test.tsx
@@ -1,18 +1,37 @@
+import { createTheme } from "@mui/material";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import TextfieldEdit from "../TextfieldEdit";
 
 //  cleanup after each test case
 afterEach(cleanup);
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff6200",
+    },
+    secondary: {
+      main: "#5a534f",
+    },
+    text: {
+      primary: "#5a534f",
+    },
+  },
+});
+
 //  test rendering of the editable textfield and check of bodytext
 it("Rendering without crash", () => {
-  const { getByText } = render(<TextfieldEdit text="lorem ipsum" />);
+  const { getByText } = render(
+    <TextfieldEdit text="lorem ipsum" theme={theme} />
+  );
   expect(getByText("lorem ipsum")).toBeInTheDocument();
 });
 
 //  test the change of the bodytext after editing in the textfield
 it("Check input change", () => {
-  const { getByText } = render(<TextfieldEdit text="lorem ipsum" />);
+  const { getByText } = render(
+    <TextfieldEdit text="lorem ipsum" theme={theme} />
+  );
   const inputElement = getByText("lorem ipsum") as HTMLInputElement;
   fireEvent.change(inputElement, { target: { value: "lorem ipsum changed" } });
   expect(getByText("lorem ipsum changed")).toBeInTheDocument();
