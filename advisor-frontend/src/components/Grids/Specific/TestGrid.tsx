5d513d6630d898598848122bcde60f3ba4eb97ee
Adrian <a.a.t.beers@student.tue.nl>
2022-06-15 17:36:28 +0200

    Implement data fetch in TestGrid

--- advisor-frontend/src/components/Grids/Specific/TestGrid.tsx
+++ advisor-frontend/src/components/Grids/Specific/TestGrid.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import { QueryKey, useQuery } from "react-query";
+import axios from "axios";
 
 import { GridColumns } from "@mui/x-data-grid";
 import { Theme } from "@mui/material";
+
 import GenericGrid from "../Generic/GenericGrid";
 
 // Define type for the rows in the grid
@@ -12,14 +15,27 @@ type Row = {
   body: string;
 };
 
-export default function AssessmentCompletedGrid({ theme }: { theme: Theme }) {
+function useData(key: QueryKey, url: string) {
+  return useQuery(key, async () => {
+    const { data } = await axios.get(url);
+    return data;
+  });
+}
+
+export default function TestGrid({ theme }: { theme: Theme }) {
   const [rows, setRows] = React.useState<Row[]>([]);
+
+  const { status, data, error, isFetching } = useData(
+    ["parameter 1", "parameter 2"],
+    "https://jsonplaceholder.typicode.com/posts"
+  );
 
   // Fetch initial rows of the grid
   React.useEffect(() => {
-    // TODO Replace this by API fetch
-    setRows(() => []);
-  }, []);
+    if (status === "success") {
+      setRows(() => data);
+    }
+  }, [status, data, error, isFetching]);
 
   const columns = React.useMemo<GridColumns<Row>>(
     () => [
