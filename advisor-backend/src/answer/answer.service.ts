9d47bf7dbff1b82e75cf401135e0c2abb559f292
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-15 18:09:51 +0200

    Solve import issue

--- advisor-backend/src/answer/answer.service.ts
+++ advisor-backend/src/answer/answer.service.ts
@@ -4,7 +4,7 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { UpdateAnswerDto } from './dto/update-answer.dto';
 
 @Injectable()
