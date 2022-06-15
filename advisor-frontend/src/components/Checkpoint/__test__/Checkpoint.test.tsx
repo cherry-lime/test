eef71e3e1be04c4869626de11aa43ab3e8581bb7
ian100000 <ianvandewetering@gmail.com>
2022-06-15 16:43:35 +0200

    fix issues in testing
    fix issues in testing again

--- advisor-frontend/src/components/Checkpoint/__test__/Checkpoint.test.tsx
+++ advisor-frontend/src/components/Checkpoint/__test__/Checkpoint.test.tsx
@@ -13,13 +13,24 @@ afterEach(cleanup);
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#ff6200",
+      light: "#FFD6B1", // Light Orange
+      main: "#FF6200", // Orange
+      dark: "#AA3909", // Dark Orange
     },
     secondary: {
-      main: "#5a534f",
+      light: "#EDE6E2", // Light Grey
+      main: "#8B817C", // Grey
+      dark: "#5A534F", // Dark Grey
     },
     text: {
-      primary: "#5a534f",
+      secondary: "#5A534F", // Dark Grey
+    },
+    info: {
+      light: "#FAF6F3", // Lightest Grey
+      main: "#ffffff", // White color for icons.
+    },
+    background: {
+      default: "#ffffff", // Used to define the custom sidebar text color.
     },
   },
 });
