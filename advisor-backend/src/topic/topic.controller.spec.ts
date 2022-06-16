495463c6a3717be3274749a0c671a3273913ab11
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-16 09:19:35 +0200

    Add mocks to tests

--- advisor-backend/src/topic/topic.controller.spec.ts
+++ advisor-backend/src/topic/topic.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TopicController } from './topic.controller';
 import { TopicService } from './topic.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('TopicController', () => {
   let controller: TopicController;
@@ -8,8 +11,20 @@ describe('TopicController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TopicController],
-      providers: [TopicService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === TopicService) {
+          return {};
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<TopicController>(TopicController);
   });
--- advisor-backend/src/topic/topic.service.spec.ts
+++ advisor-backend/src/topic/topic.service.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { mockPrisma } from '../prisma/mock/mockPrisma';
+import { PrismaService } from '../prisma/prisma.service';
 import { TopicService } from './topic.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('TopicService', () => {
   let service: TopicService;
@@ -7,7 +12,20 @@ describe('TopicService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [TopicService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === PrismaService) {
+          return mockPrisma;
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     service = module.get<TopicService>(TopicService);
   });
