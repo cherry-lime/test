8e2155be7aeedd6a6d3996d073efaed2140a8a06
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-17 12:42:28 +0200

    Add constraints to answer_weight

--- advisor-backend/src/answer/dto/answer.dto.ts
+++ advisor-backend/src/answer/dto/answer.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, Max, Min } from 'class-validator';
 
 export class AnswerDto {
   @ApiProperty({ default: 'New Answer' })
   answer_text: string;
 
   @ApiProperty()
+  @IsNumber()
+  @Min(0)
+  @Max(100)
   answer_weight: number;
 
   @ApiProperty()
