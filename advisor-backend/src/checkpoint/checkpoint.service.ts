701579cec61d919d66d25a92b528124ac00683fe
Aditya-Raghunath <a.raghunath@student.tue.nl>
2022-06-17 11:50:41 +0200

    checkpoint service modified

--- advisor-backend/src/checkpoint/checkpoint.service.ts
+++ advisor-backend/src/checkpoint/checkpoint.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+
 import { CreateCheckpointDto } from './dto/create-checkpoint.dto';
 import { UpdateCheckpointDto } from './dto/update-checkpoint.dto';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 @Injectable()
 export class CheckpointService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createCheckpointDto: CreateCheckpointDto) {
     return 'This action adds a new checkpoint';
   }
@@ -20,7 +28,53 @@ export class CheckpointService {
     return `This action updates a #${id} checkpoint`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} checkpoint`;
+  /**
+   * Delete checkpoint
+   * @param checkpoint_id checkpoint id
+   * @returns deleted checkpoint
+   * @throws checkpoint not found
+   */
+   async delete(checkpoint_id: number) {
+    // Get checkpoint by id from prisma
+    const checkpoint = await this.prisma.checkpoint.findUnique({
+      where: {
+        checkpoint_id,
+      },
+    });
+
+    // Throw NotFoundException if checkpoint not found
+    if (!checkpoint) {
+      throw new NotFoundException('checkpoint not found');
+    }
+
+    // Decrement order of all categories with order bigger than deleted checkpoint
+    await this.prisma.checkpoint.updateMany({
+      where: {
+        category_id: checkpoint.category_id,
+        order: {
+          gte: checkpoint.order,
+        },
+      },
+      data: {
+        order: {
+          decrement: 1,
+        },
+      },
+    });
+
+    // Delete checkpoint
+    return await this.prisma.checkpoint
+      .delete({
+        where: {
+          checkpoint_id,
+        },
+      })
+      .catch((error) => {
+        // Throw error if category not found
+        if (error.code === 'P2025') {
+          throw new NotFoundException('checkpoint not found');
+        }
+        throw new InternalServerErrorException();
+      });
   }
 }
