8579b1ac24e0a293c1abe76ef0eb93e1b8e32b41
Dragonsky8 <Williamiphone3@gmail.com>
2022-06-18 12:46:41 +0200

    Fix Prettier issues

--- advisor-frontend/src/pages/user/UserInterface/UserInterface.tsx
+++ advisor-frontend/src/pages/user/UserInterface/UserInterface.tsx
@@ -1,14 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
-import AssessmentIcon from "@mui/icons-material/Assessment";
-import GroupsIcon from "@mui/icons-material/Groups";
-import BarChartIcon from '@mui/icons-material/BarChart';
-import ShowChartIcon from "@mui/icons-material/ShowChart";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import PageLayout from "../../PageLayout";
 import userTypes from "../../../components/Sidebar/listUsersTypes";
-import PageCard from "../../../components/PageCard/PageCard";
-import testimageFigma from "../../../components/PageCard/testimageFigma.png";
-import {TeamCard, EvaluationCard, ProgressCard, NotificationCard} from "../../../components/PageCard/SpecificPageCards";
+import {
+  TeamCard,
+  EvaluationCard,
+  ProgressCard,
+  NotificationCard,
+} from "../../../components/PageCard/SpecificPageCards";
 
 /**
  * Home page visible to anyone with the user role
@@ -23,21 +21,21 @@ function UserInterface() {
         <EvaluationCard />
       </Link>
       <Link to="/teams" state={data} data-testid="user-teams">
-        <TeamCard/>
+        <TeamCard />
       </Link>
       <Link
         to="/user/self_evaluations/:assessmentId"
         state={data}
         data-testid="user-progress"
       >
-        <ProgressCard/>
+        <ProgressCard />
       </Link>
       <Link
         to="/user/self_evaluations/:assessmentId"
         state={data}
         data-testid="user-progress"
       >
-        <NotificationCard/>
+        <NotificationCard />
       </Link>
 
       <h3>Notifications</h3>
