4e6061f095c8a352fea51084a3cf1619a46d65f9
Chiara <c.liotta@student.tue.nl>
2022-06-19 16:00:05 +0200

    update admin interface cards

--- advisor-frontend/src/pages/admin/AdminInterface/AdminInterface.tsx
+++ advisor-frontend/src/pages/admin/AdminInterface/AdminInterface.tsx
@@ -1,9 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
 import AssessmentIcon from "@mui/icons-material/Assessment";
 import PageCard from "../../../components/PageCard/PageCard";
-import testimageFigma from "../../../components/PageCard/testimageFigma.png";
 import userType from "../../../components/Sidebar/listUsersTypes";
 import PageLayout from "../../PageLayout";
+import IndividualCard from "../../../components/PageCard/SpecificPageCards/IndividualCard";
+import TemplateCard from "../../../components/PageCard/SpecificPageCards/TemplateCard";
 
 /**
  * Home page visible to anyone with the admin role
@@ -18,29 +19,13 @@ function AdminInterface() {
         <h2> Admin Interface </h2>
 
         <Link to="/admin/individuals" state={data} data-testid="individuals">
-          <PageCard
-            bodyText="View and edit list of individuals"
-            headerText="Individuals"
-            cardHeight="15vh"
-            // eslint-disable-next-line react/jsx-no-undef
-            icon={<AssessmentIcon color="primary" fontSize="large" />}
-            image={testimageFigma}
-            isImageLeft
-          />
+          <IndividualCard/>
         </Link>
 
         <br />
 
         <Link to="/admin/templates" state={data} data-testid="templates">
-          <PageCard
-            bodyText="View, create, and edit evaluation templates"
-            headerText="Templates"
-            cardHeight="15vh"
-            // eslint-disable-next-line react/jsx-no-undef
-            icon={<AssessmentIcon color="primary" fontSize="large" />}
-            image={testimageFigma}
-            isImageRight
-          />
+          <TemplateCard/>
         </Link>
       </PageLayout>
     </div>
