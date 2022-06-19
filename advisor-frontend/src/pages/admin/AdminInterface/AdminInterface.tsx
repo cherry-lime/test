e2f638cafb5222c6c2e96ecbaa3e2aec736725c3
Chiara <c.liotta@student.tue.nl>
2022-06-19 13:57:31 +0200

    home page admin

--- advisor-frontend/src/pages/admin/AdminInterface/AdminInterface.tsx
+++ advisor-frontend/src/pages/admin/AdminInterface/AdminInterface.tsx
@@ -1,4 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import PageCard from "../../../components/PageCard/PageCard";
+import testimageFigma from "../../../components/PageCard/testimageFigma.png";
+import userType from "../../../components/Sidebar/listUsersTypes";
+import PageLayout from "../../PageLayout";
 
 /**
  * Home page visible to anyone with the admin role
@@ -9,18 +14,35 @@ function AdminInterface() {
 
   return (
     <div>
-      <h2> Admin Interface </h2>
-      <Link to="/admin/individuals" state={data} data-testid="individuals">
-        {" "}
-        Individuals{" "}
-      </Link>
+      <PageLayout title="Home" footer sidebarType={userType.ADMIN}>
+        <h2> Admin Interface </h2>
 
-      <br />
+        <Link to="/admin/individuals" state={data} data-testid="individuals">
+          <PageCard
+            bodyText="View and edit list of individuals"
+            headerText="Individuals"
+            cardHeight="15vh"
+            // eslint-disable-next-line react/jsx-no-undef
+            icon={<AssessmentIcon color="primary" fontSize="large" />}
+            image={testimageFigma}
+            isImageLeft
+          />
+        </Link>
 
-      <Link to="/admin/templates" state={data} data-testid="templates">
-        {" "}
-        Templates{" "}
-      </Link>
+        <br />
+
+        <Link to="/admin/templates" state={data} data-testid="templates">
+          <PageCard
+            bodyText="View, create, and edit evaluation templates"
+            headerText="Templates"
+            cardHeight="15vh"
+            // eslint-disable-next-line react/jsx-no-undef
+            icon={<AssessmentIcon color="primary" fontSize="large" />}
+            image={testimageFigma}
+            isImageRight
+          />
+        </Link>
+      </PageLayout>
     </div>
   );
 }
--- advisor-frontend/src/pages/admin/ListOfIndividuals/ListOfIndividuals.tsx
+++ advisor-frontend/src/pages/admin/ListOfIndividuals/ListOfIndividuals.tsx
@@ -1,4 +1,8 @@
 import { Link, useLocation } from "react-router-dom";
+import PageLayout from "../../PageLayout";
+import userType from "../../../components/Sidebar/listUsersTypes";
+
+
 
 /**
  * Page listing all users registered in the tool
@@ -10,13 +14,16 @@ function ListOfIndividuals() {
 
   return (
     <div>
-      <p> {data} view </p>
-      <Link to="/admin" state={data}>
-        {" "}
-        Go Back to Admin Interface{" "}
-      </Link>
+      <PageLayout title="Individuals" footer sidebarType={userType.ADMIN}>
+        <p> {data} view </p>
+        <Link to="/admin" state={data}>
+          {" "}
+          Go Back to Admin Interface{" "}
+        </Link>
 
       <h2> List of Individuals</h2>
+      </PageLayout>
+      
     </div>
   );
 }
