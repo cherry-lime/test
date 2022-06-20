747ffe0d6fe82a88b699c005e91b74104fb6ceb6
Chiara <c.liotta@student.tue.nl>
2022-06-20 21:40:18 +0200

    add dropdown for active templates

--- advisor-frontend/src/pages/admin/templates/ListOfTemplates/ListOfTemplates.tsx
+++ advisor-frontend/src/pages/admin/templates/ListOfTemplates/ListOfTemplates.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from "react";
+import { FormControl, MenuItem, Select } from "@mui/material";
 import userType from "../../../../components/Sidebar/listUsersTypes";
 import Theme from "../../../../Theme";
 import PageLayout from "../../../PageLayout";
@@ -8,13 +10,60 @@ import TemplateGrid from "../../../../components/Grids/Specific/TemplateGrid";
  * This should only be accessible to admins
  */
 function ListOfTemplates() {
+  const [activeIndividual, setActiveIndividual] = useState("123");
+  const [activeTeam, setActiveTeam] = useState("abc");
+  const individualTemplateList = ["123", "456", "789"];
+  const teamTemplateList = ["abc", "def", "ghi"];
+
+  const handleActiveIndividualChange = useCallback(
+    (event) => {
+      setActiveIndividual(event.target.value);
+    },
+    [activeIndividual]
+  );
+
+  const handleActiveTeamChange = useCallback(
+    (event) => {
+      setActiveTeam(event.target.value);
+    },
+    [activeTeam]
+  );
+
   return (
     <div>
       <PageLayout title="Templates" sidebarType={userType.ADMIN}>
-        <h2>Individual Templates</h2>
-        <TemplateGrid theme={Theme} assessmentType="INDIVIDUAL" />
-        <h2>Team Templates</h2>
-        <TemplateGrid theme={Theme} assessmentType="TEAM" />
+        <div style={{ width: "inherit" }}>
+          <h2>Individual Templates</h2>
+          <p>Active template for individual evaluations:</p>
+          <FormControl sx={{ m: 1, width: 200, marginBottom: "20px" }}>
+            <Select
+              value={activeIndividual}
+              onChange={handleActiveIndividualChange}
+            >
+              {individualTemplateList.map((t) => (
+                <MenuItem key={t} value={t}>
+                  {t}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TemplateGrid theme={Theme} assessmentType="INDIVIDUAL" />
+        </div>
+
+        <div style={{ width: "inherit" }}>
+          <h2>Team Templates</h2>
+          <p>Active template for team evaluations:</p>
+          <FormControl sx={{ m: 1, width: 200, marginBottom: "20px" }}>
+            <Select value={activeTeam} onChange={handleActiveTeamChange}>
+              {teamTemplateList.map((t) => (
+                <MenuItem key={t} value={t}>
+                  {t}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TemplateGrid theme={Theme} assessmentType="TEAM" />
+        </div>
       </PageLayout>
     </div>
   );
