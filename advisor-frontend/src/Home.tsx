0937b1af9e4bf48da378cbae308733f7cfab90fe
Dragonsky8 <Williamiphone3@gmail.com>
2022-06-20 18:00:32 +0200

    Create redux setup. Added userID to the global state

--- advisor-frontend/src/Home.tsx
+++ advisor-frontend/src/Home.tsx
@@ -1,15 +1,22 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ExampleButton from "./components/ExampleButton/ExampleButton";
 import Greetings from "./components/SampleText/SampleText";
 import logo from "./logo.svg";
 import "./App.css";
+import { RootState } from "./app/store";
+import { setUserID } from "./app/userDataSlice";
 
 function UserInterface() {
+  const { userID } = useSelector((state: RootState) => state.userData);
+  const dispatch = useDispatch();
   return (
     <div className="App-header">
-      <h1>This is the home page</h1>
+      <h1>This is the home page with {userID}</h1>
       Hello World
       <Greetings name="This is a test to test a component" />
       <ExampleButton name="Buttontext" />
+      <button type="button" onClick={() => dispatch(setUserID("ttt"))} > click to change ID</button>
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
