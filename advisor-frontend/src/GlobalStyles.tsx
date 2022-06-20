38b3de6021bc65a381d7863bb719b7835c3ec380
Chiara <c.liotta@student.tue.nl>
2022-06-20 11:44:29 +0200

    Update GlobalStyles.tsx

--- advisor-frontend/src/GlobalStyles.tsx
+++ advisor-frontend/src/GlobalStyles.tsx
@@ -6,8 +6,6 @@ const GlobalStyles = createGlobalStyle`
     color: ${Theme.palette.text.secondary};
     width: inherit;
     text-align: left;
-    float: left;
-    cursor: pointer;
     margin-bottom: 0px;
   }
 `;
