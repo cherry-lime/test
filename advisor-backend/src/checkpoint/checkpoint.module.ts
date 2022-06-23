e354ca07338c7879d335de56ebe003bb7792dbe7
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-23 10:15:38 +0200

    Fix import error

--- advisor-backend/src/checkpoint/checkpoint.module.ts
+++ advisor-backend/src/checkpoint/checkpoint.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CheckpointService } from './checkpoint.service';
 import { CheckpointController } from './checkpoint.controller';
 import { TemplateService } from '../template/template.service';
-import { TopicService } from 'src/topic/topic.service';
+import { TopicService } from '../topic/topic.service';
 
 @Module({
   controllers: [CheckpointController],
