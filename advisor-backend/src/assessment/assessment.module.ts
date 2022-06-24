9003286c73329eaa5974ada807e2b1fbffdb2b14
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-24 12:35:35 +0200

    Fix errors

--- advisor-backend/src/assessment/assessment.module.ts
+++ advisor-backend/src/assessment/assessment.module.ts
@@ -4,9 +4,18 @@ import { AssessmentController } from './assessment.controller';
 import { TemplateService } from '../template/template.service';
 import { FeedbackService } from '../feedback/feedback.service';
 import { SaveService } from '../save/save.service';
+import { CheckpointService } from '../checkpoint/checkpoint.service';
+import { TopicService } from '../topic/topic.service';
 
 @Module({
   controllers: [AssessmentController],
-  providers: [AssessmentService, FeedbackService, TemplateService, SaveService],
+  providers: [
+    AssessmentService,
+    FeedbackService,
+    TemplateService,
+    SaveService,
+    CheckpointService,
+    TopicService,
+  ],
 })
 export class AssessmentModule {}
