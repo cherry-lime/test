739632da346d986a05664f29ac8098ad6b6bb881
omarsalemm_ <omar.ksalem02@gmail.com>
2022-06-24 22:15:01 +0200

    lintfix

--- advisor-frontend/src/app/loginAPI.tsx
+++ advisor-frontend/src/app/loginAPI.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
 import axios from "axios";
 import { useMutation } from "react-query";
 import { useDispatch } from "react-redux";
