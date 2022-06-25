f8c141277fe8686a56e20b8bc4cdf37a5044b056
ian100000 <ianvandewetering@gmail.com>
2022-06-25 20:17:32 +0200

    Update TeamList.tsx
    make textfield + button + make textfield and button based on non-admin roles

--- advisor-frontend/src/pages/teams/TeamList/TeamList.tsx
+++ advisor-frontend/src/pages/teams/TeamList/TeamList.tsx
@@ -4,6 +4,8 @@ import PageLayout from "../../PageLayout";
 import userTypes from "../../../components/Sidebar/listUsersTypes";
 import TeamGrid from "../../../components/Grids/Specific/Team/TeamGrid";
 import { RootState } from "../../../app/store";
+import TextfieldEdit from "../../../components/TextfieldEdit/TextfieldEdit";
+import ButtonRegular from "../../../components/ButtonRegular/ButtonRegular";
 
 /**
  * Page with the list of teams that the user or assessor is part of
@@ -13,6 +15,19 @@ function TeamList({ theme }: { theme: Theme }) {
 
   return (
     <PageLayout title="Teams" sidebarType={userTypes[userRole]}>
+      {userRole !== "ADMIN" && (
+        <TextfieldEdit text="Enter token here" theme={theme} rows={1} />
+      )}
+      {userRole !== "ADMIN" && (
+        <div
+          style={{
+            width: "inherit",
+          }}
+        >
+          <ButtonRegular text="Join Team With Token" />
+        </div>
+      )}
+
       <TeamGrid theme={theme} userRole={userRole} />
     </PageLayout>
   );
