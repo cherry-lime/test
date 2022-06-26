1eab13d53a9ef0aa8cd9aa044752dd121aad90d1
Paul <p.r.vlaswinkel@student.tue.nl>
2022-06-26 21:47:42 +0200

    Update authentication.dto.ts

--- advisor-backend/src/auth/dto/authentication.dto.ts
+++ advisor-backend/src/auth/dto/authentication.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { AssessmentParticipants, Role, UserInTeam } from '@prisma/client';
-import { IsIn, IsString, IsUUID } from 'class-validator';
+import { IsIn, IsString } from 'class-validator';
 
 /**
  * Response with authentication information
