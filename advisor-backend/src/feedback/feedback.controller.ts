111c97296b106b9dab0bd644489e4a141986936e
Vlaswinkel <p.r.vlaswinkel@student.tue.nl>
2022-06-26 14:04:34 +0200

    Restrict recommendation editting to assessor and admin

--- advisor-backend/src/feedback/feedback.controller.ts
+++ advisor-backend/src/feedback/feedback.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Param, ParseIntPipe, Patch } from '@nestjs/common';
 import { ApiNotFoundResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+import { Roles } from '../common/decorators/roles.decorator';
 import { RecommendationDto } from './dto/recommendation.dto';
 import { UpdateRecommendationDto } from './dto/UpdateRecommendation.dto';
 import { FeedbackService } from './feedback.service';
@@ -10,6 +12,7 @@ export class FeedbackController {
   constructor(private readonly feedbackService: FeedbackService) {}
 
   @Patch(':feedback_id')
+  @Roles(Role.ASSESSOR, Role.ADMIN)
   @ApiResponse({
     description: 'Updated feedback',
     type: RecommendationDto,
