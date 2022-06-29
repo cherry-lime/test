f141e79940b7ecf8d34356aca748dcb27008ad89
Dragonsky8 <Williamiphone3@gmail.com>
2022-06-29 08:59:41 +0200

    Add key to the <p> tags, which deepscan detected

--- advisor-frontend/src/components/PageCard/SpecificPageCards/ProgressEvaluationCard.tsx
+++ advisor-frontend/src/components/PageCard/SpecificPageCards/ProgressEvaluationCard.tsx
@@ -330,7 +330,7 @@ export default function ProgressEvaluationCard({
                 );
 
                 if (displayedObject) {
-                  return <p>{`${displayedObject.name}: ${score.score}%`}</p>;
+                  return <p key={displayedObject.id}>{`${displayedObject.name}: ${score.score}%`}</p>;
                 }
 
                 return "";
