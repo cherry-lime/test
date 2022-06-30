3c48f4cdba969dcabf7957cee274db2ecc5c4a2d
Adrian <a.a.t.beers@student.tue.nl>
2022-06-30 15:54:44 +0200

    Implement tests for helpers

--- advisor-frontend/src/components/Grids/functions/helpers/__test__/helpers.test.tsx
+++ advisor-frontend/src/components/Grids/functions/helpers/__test__/helpers.test.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-import { GridColDef } from "@mui/x-data-grid";
 import * as helpers from "../helpers";
 
 describe("Testing helper function", () => {
@@ -9,14 +7,158 @@ describe("Testing helper function", () => {
     name: string;
   };
 
-  it("addRow should add row to state", () => {
-    const rows: Row[] = [{ id: 1, order: 1, name: "Clark Kent" }];
-
+  it("initRows should sort on order", () => {
     expect(
-      helpers.addRow(rows, { id: 2, order: 2, name: "Bruce Wayne" })
+      helpers.initRows([
+        { id: 2, order: 2, name: "Bruce Wayne" },
+        { id: 1, order: 1, name: "Clark Kent" },
+      ])
     ).toStrictEqual([
       { id: 1, order: 1, name: "Clark Kent" },
       { id: 2, order: 2, name: "Bruce Wayne" },
     ]);
   });
+
+  it("updateOrderRows should update order based on index", () => {
+    expect(
+      helpers.updateOrderRows([
+        { id: 2, order: 2, name: "Bruce Wayne" },
+        { id: 1, order: 1, name: "Clark Kent" },
+      ])
+    ).toStrictEqual([
+      { id: 2, order: 1, name: "Bruce Wayne" },
+      { id: 1, order: 2, name: "Clark Kent" },
+    ]);
+  });
+
+  it("moveRow should move row and update order", () => {
+    expect(
+      helpers.moveRow(
+        [
+          { id: 1, order: 1, name: "Bruce Wayne" },
+          { id: 2, order: 2, name: "Clark Kent" },
+          { id: 3, order: 3, name: "Tony Stark" },
+        ],
+        { id: 1, order: 1, name: "Bruce Wayne" },
+        3
+      )
+    ).toStrictEqual([
+      { id: 2, order: 1, name: "Clark Kent" },
+      { id: 3, order: 2, name: "Tony Stark" },
+      { id: 1, order: 3, name: "Bruce Wayne" },
+    ]);
+  });
+
+  it("moveRow should not move row to order below bounds", () => {
+    expect(
+      helpers.moveRow(
+        [
+          { id: 1, order: 1, name: "Bruce Wayne" },
+          { id: 2, order: 2, name: "Clark Kent" },
+          { id: 3, order: 3, name: "Tony Stark" },
+        ],
+        { id: 1, order: 1, name: "Bruce Wayne" },
+        0
+      )
+    ).toStrictEqual([
+      { id: 1, order: 1, name: "Bruce Wayne" },
+      { id: 2, order: 2, name: "Clark Kent" },
+      { id: 3, order: 3, name: "Tony Stark" },
+    ]);
+  });
+
+  it("moveRow should not move row to order above bounds", () => {
+    expect(
+      helpers.moveRow(
+        [
+          { id: 1, order: 1, name: "Bruce Wayne" },
+          { id: 2, order: 2, name: "Clark Kent" },
+          { id: 3, order: 3, name: "Tony Stark" },
+        ],
+        { id: 1, order: 1, name: "Bruce Wayne" },
+        4
+      )
+    ).toStrictEqual([
+      { id: 1, order: 1, name: "Bruce Wayne" },
+      { id: 2, order: 2, name: "Clark Kent" },
+      { id: 3, order: 3, name: "Tony Stark" },
+    ]);
+  });
+
+  it("updateRow should replace oldRow with newRow", () => {
+    expect(
+      helpers.updateRow(
+        [
+          { id: 1, order: 1, name: "Bruce Wayne" },
+          { id: 2, order: 2, name: "Clark Kent" },
+        ],
+        { id: 1, order: 1, name: "Tony Stark" },
+        { id: 1, order: 1, name: "Bruce Wayne" }
+      )
+    ).toStrictEqual([
+      { id: 1, order: 1, name: "Tony Stark" },
+      { id: 2, order: 2, name: "Clark Kent" },
+    ]);
+  });
+
+  it("updateRow should replace oldRow with newRow with order", () => {
+    expect(
+      helpers.updateRow(
+        [
+          { id: 1, order: 1, name: "Bruce Wayne" },
+          { id: 2, order: 2, name: "Clark Kent" },
+        ],
+        { id: 1, order: 2, name: "Bruce Wayne" },
+        { id: 1, order: 1, name: "Bruce Wayne" }
+      )
+    ).toStrictEqual([
+      { id: 2, order: 1, name: "Clark Kent" },
+      { id: 1, order: 2, name: "Bruce Wayne" },
+    ]);
+  });
+
+  it("addRow should add row", () => {
+    expect(
+      helpers.addRow([{ id: 1, order: 1, name: "Clark Kent" }], {
+        id: 2,
+        order: 2,
+        name: "Bruce Wayne",
+      })
+    ).toStrictEqual([
+      { id: 1, order: 1, name: "Clark Kent" },
+      { id: 2, order: 2, name: "Bruce Wayne" },
+    ]);
+  });
+
+  it("deleteRow should delete row", () => {
+    expect(
+      helpers.deleteRow(
+        [
+          { id: 1, order: 1, name: "Clark Kent" },
+          { id: 2, order: 2, name: "Bruce Wayne" },
+        ],
+        {
+          id: 2,
+          order: 2,
+          name: "Bruce Wayne",
+        }
+      )
+    ).toStrictEqual([{ id: 1, order: 1, name: "Clark Kent" }]);
+  });
+});
+
+it("deleteRow should delete row and update order", () => {
+  expect(
+    helpers.deleteRow(
+      [
+        { id: 1, order: 1, name: "Clark Kent" },
+        { id: 2, order: 2, name: "Bruce Wayne" },
+      ],
+      {
+        id: 1,
+        order: 1,
+        name: "Clark Kent",
+      }
+    )
+  ).toStrictEqual([{ id: 2, order: 1, name: "Bruce Wayne" }]);
 });
