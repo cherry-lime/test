d2695f6c099de2074c92ebf11822f7678ca06bb9
ian100000 <ianvandewetering@gmail.com>
2022-07-02 18:08:06 +0200

    code commenting and api updates for prod + dev code
    code commenting

--- advisor-frontend/src/api/_API.tsx
+++ advisor-frontend/src/api/_API.tsx
@@ -3,9 +3,16 @@ import axios from "axios";
 /**
  * Create axios instance to be used in the rest of API
  */
-const API = axios.create({
-  withCredentials: true,
-  baseURL: "https://tabackend.azurewebsites.net",
-});
+
+const API =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? axios.create({
+        withCredentials: true,
+        baseURL: "http://localhost:5000/",
+      })
+    : axios.create({
+        withCredentials: true,
+        baseURL: "https://tabackend.azurewebsites.net",
+      });
 
 export default API;
