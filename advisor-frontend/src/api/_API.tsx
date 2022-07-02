d101b55243d39921f3e731f36479d638a52f443c
ian100000 <ianvandewetering@gmail.com>
2022-07-02 20:24:13 +0200

    Update _API.tsx

--- advisor-frontend/src/api/_API.tsx
+++ advisor-frontend/src/api/_API.tsx
@@ -2,6 +2,10 @@ import axios from "axios";
 
 /**
  * Create axios instance to be used in the rest of API
+ * In the constant declaration of the API
+ * Two different baseURL's are used:
+ * http://localhost:5000/ is used for development mode,
+ * https://tabackend.azurewebsites.net is used for production mode
  */
 
 const API =
