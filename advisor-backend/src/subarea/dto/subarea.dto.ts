a10b37327862fb8a97b4c525032e063c25f1be3f
lukasagurkas <57620511+lukasagurkas@users.noreply.github.com>
2022-07-03 23:59:24 +0200

    Code commenting everything except auth and user related files

--- advisor-backend/src/subarea/dto/subarea.dto.ts
+++ advisor-backend/src/subarea/dto/subarea.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Min } from 'class-validator';
 
+// Data Transfer Object (DTO) for subarea
+// ApiProperty decorator is used to define Swagger UI properties for this class
+// Default value is used for swagger UI as example values
+// Min decorator is used to define the minimum value for the property
 export class SubareaDto {
   @ApiProperty({ default: 1 })
   subarea_id: number;
@@ -20,6 +24,7 @@ export class SubareaDto {
   @ApiProperty({ default: false })
   disabled: boolean;
 
+  // Used as an importance metric for the subarea
   @ApiProperty({ default: 1 })
   @Min(1)
   order: number;
--- advisor-backend/src/template/template.module.ts
+++ advisor-backend/src/template/template.module.ts
@@ -7,6 +7,11 @@ import { TopicService } from '../topic/topic.service';
 import { AnswerService } from '../answer/answer.service';
 import { CloneTemplateService } from './clone-template.service';
 
+// NestJS Module System helps structure application code into domain areas
+//  leading to better design and improved maintainability.
+// Controllers are responsible for handling incoming requests and returning responses to the client.
+// Providers are responsible for executing business logic and returning data to the controller.
+// Module for template related routes
 @Module({
   controllers: [TemplateController],
   providers: [
